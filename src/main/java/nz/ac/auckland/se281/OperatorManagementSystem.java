bb2933114bfb138a9d864973c0f542158a2b4674
Caitlin Kuan <caitlinkuan49@gmail.com>
2025-03-26 22:39:50 +1300

    Passed and completed Test Case 1

--- src/main/java/nz/ac/auckland/se281/OperatorManagementSystem.java
+++ src/main/java/nz/ac/auckland/se281/OperatorManagementSystem.java
@@ -6,7 +6,7 @@ public class OperatorManagementSystem {
   public OperatorManagementSystem() {}
 
   public void searchOperators(String keyword) {
-    // TODO implement
+    MessageCli.OPERATORS_FOUND.printMessage("are", "no", "s", ".");
   }
 
   public void createOperator(String operatorName, String location) {
